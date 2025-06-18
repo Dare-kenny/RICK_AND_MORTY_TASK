e730596f9abde7daeb992130471a53ffd52c7532
I'm Akinrogunde Damilare <121644669+Dare-kenny@users.noreply.github.com>
2025-06-18 10:17:22 +0100

    Delete RickAndMorty directory

--- RickAndMorty/src/main/java/com/example/RickAndMorty/repositories/CharacterRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.RickAndMorty.repositories;
-
-import com.example.RickAndMorty.schemas.RickAndMortyCharacters;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CharacterRepository extends JpaRepository<RickAndMortyCharacters,Integer> {
-}
