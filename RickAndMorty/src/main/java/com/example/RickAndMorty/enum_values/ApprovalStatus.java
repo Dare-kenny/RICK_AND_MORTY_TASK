535f184b83adf0cc9d8864e809dba9e61a3fea6f
I'm Akinrogunde Damilare <121644669+Dare-kenny@users.noreply.github.com>
2025-06-19 04:47:23 +0100

    Delete RickAndMorty directory

--- RickAndMorty/src/main/java/com/example/RickAndMorty/enum_values/ApprovalStatus.java
+++
@@ -1,6 +0,0 @@
-package com.example.RickAndMorty.enum_values;
-
-public enum ApprovalStatus {
-    APPROVED,
-    NOT_APPROVED
-}
--- RickAndMorty/src/main/java/com/example/RickAndMorty/repositories/CharacterRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.RickAndMorty.repositories;
-
-import com.example.RickAndMorty.schemas.CharacterEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CharacterRepository extends JpaRepository<CharacterEntity,Integer> {
-}
